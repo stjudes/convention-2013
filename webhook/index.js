5bab5c13a3c3b4a2fdf1a5be2b764ffdf6d6ade0
James Ramsay <git@jwr.vc>
2012-06-13 22:04:48 +1000

    Hook testing

--- webhook/index.js
+++ webhook/index.js
@@ -1,3 +1,5 @@
+var exec = require("child_process").exec;
+
 var githubhook = require('./node-github-hook/index'),
   servers = {
     'convention-2013':'https://github.com/stjudes/convention-2013'
@@ -5,7 +7,10 @@ var githubhook = require('./node-github-hook/index'),
 
 var hook = githubhook(8765, servers, function (err, payload) {
   if (!err) {
-    console.log(payload);
+    //console.log(payload);
+    exec("ls -lah", function (error, stdout, stderr) {
+      content = stdout;
+    });
   } else {
     console.log(err);
   }
